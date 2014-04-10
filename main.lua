4cdaf0d1414a22979fce5ac99d15e5b36a893e2d
Luka Dornhecker <luka.dornhecker@gmail.com>
2014-04-10 07:54:41 +0200

    start working on player movement

--- main.lua
+++ main.lua
@@ -2,12 +2,14 @@ require "player"
 
 GRID_SIZE = 16
 
+p = Player.new()
+
 function love.load()
-	love.graphics.setBackgroundColor(255, 255, 255)
+	love.graphics.setBackgroundColor(253, 246, 227)
 end
 
 function drawGrid()
-	love.graphics.setColor(238, 238, 238, 255);
+	love.graphics.setColor(238, 232, 213, 255);
 	local w = love.graphics.getWidth()
 	local h = love.graphics.getHeight()
 	for x=GRID_SIZE, w, GRID_SIZE do
@@ -19,10 +21,13 @@ function drawGrid()
 end
 
 function love.draw()
+	-- Player.draw(p, 147, 161, 161)
+	Player.draw(p, 38, 139, 210)
 	drawGrid()
 end
 
 function love.update(dt)
+	Player.update(p, dt)
 end
 
 function love.keypressed(key, isrepeat)
--- player.lua
+++ player.lua
@@ -1,10 +1,33 @@
 Player = {}
 function Player.new()
 	return {
-		Points = {{0, 1}, {0, 2}, {0, 3}}
+		Points = {{0, 1}, {0, 2}, {0, 3}},
+		Speed = 5
 	}
 end
 
-function Player.draw(player, color)
-	
+function Player.draw(player, r, g, b)
+	love.graphics.setColor(r, g, b)
+	for _, p in ipairs(player.Points) do
+		local x = math.floor(p[1])
+		local y = math.floor(p[2])
+		love.graphics.rectangle("fill", x * GRID_SIZE, y * GRID_SIZE, GRID_SIZE, GRID_SIZE)
+	end
+end
+
+function Player.update(player, dt)
+	local i = #player.Points
+	-- print(i)
+	-- while i > 2 do
+		-- print(player.Points[i][1], player.Points[i][2])
+		-- player.Points[i][1] = player.Points[i-1][1]
+		-- player.Points[i][2] = player.Points[i-1][2]
+		-- i = i - 1
+	-- end
+	player.Points[3][1] = player.Points[2][1]
+	player.Points[3][2] = player.Points[2][2]
+	player.Points[2][1] = math.floor(player.Points[1][1])
+	player.Points[2][2] = math.floor(player.Points[1][2])
+
+	player.Points[1][1] = player.Points[1][1] + player.Speed * dt
 end

8a2315917f5710c244541196cbe5c3ec9cbaa884
Luka Dornhecker <luka.dornhecker@gmail.com>
2017-05-29 18:42:07 +0200

    dont let candy spawn on border cells

--- game.lua
+++ game.lua
@@ -15,38 +15,39 @@ function Game:my_draw()
   for _, candy in ipairs(Game.candys) do
     candy:draw()
   end
-  Game.p:draw(255, 255, 255)
+  -- self:draw_score()
+  self.p:draw(255, 255, 255)
   util.draw_grid()
 end
 
 function Game:update(dt)
-  Game:check_collisions()
+  self:check_collisions()
   Game.p:update(dt)
 end
 
 function Game:keypressed(key, isrepeat)
-  Game.p:keypressed(key, isrepeat)
+  self.p:keypressed(key, isrepeat)
 end
 
 function Game:add_candy()
-  local x = love.math.random(WIDTH_GRID) - 1
-  local y = love.math.random(HEIGHT_GRID) - 1
-  table.insert(Game.candys, Candy:new({x, y}))
+  local x = 1 + love.math.random(WIDTH_GRID - 3)
+  local y = 1 + love.math.random(HEIGHT_GRID - 3)
+  table.insert(self.candys, Candy:new({x, y}))
 end
 
 function Game:check_collisions()
-  if not Game.p.alive then return end
+  if not self.p.alive then return end
 
   for i, candy in ipairs(Game.candys) do
-    if Game.p.points[1][1] == candy.x and Game.p.points[1][2] == candy.y then
+    if self.p.points[1][1] == candy.x and self.p.points[1][2] == candy.y then
       table.remove(Game.candys, i)
-      Game.p:grow(candy.worth)
-      Game:add_candy()
+      self.p:grow(candy.worth)
+      self:add_candy()
     end
   end
 
-  if Game.p:collides_with_self() then
-    Game.p:die()
+  if self.p:collides_with_self() then
+    self.p:die()
   end
 end
 
